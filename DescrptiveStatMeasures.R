fe33a0429b3331b18507629208ad298c3cd0f9b9
Jjackson-r <joeljackson@claflin.edu>
2026-08-18 16:16:36 -0400

    Add files via upload

---
+++ DescrptiveStatMeasures.R
@@ -0,0 +1,15 @@
+#Name: Joel, Date:08/18/2026, Purpose: To test functions of descriptive
+#statistics
+
+#Create a variable called variable 1 and store 5 numbers from 1-5
+
+variable1 <- c(1,2,3,4,5)
+
+# calculate mean of variable1
+
+mean(variable1)
+[1]3
+
+# calculate standard deviation
+sd(variable1)
+[1] 1.581139
